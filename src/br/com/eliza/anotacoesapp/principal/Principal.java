71dc197972ecf22459ed0b276692be9cffc9d412
elizauema <eliza.uema@gmail.com>
2020-09-03 20:17:19 -0300

    inclusao do try catch

--- src/br/com/eliza/anotacoesapp/principal/Principal.java
+++ src/br/com/eliza/anotacoesapp/principal/Principal.java
@@ -1,17 +1,27 @@
 
 package br.com.eliza.anotacoesapp.principal;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import br.com.eliza.anotacoesapp.menu.Menu;
 
 public class Principal {
-	public static void main(String[] args ) throws IOException {
-		
-		String caminhoArquivoJson = "C:\\Users\\eliza\\eclipse-workspace\\anotacao\\anotacoes\\anotacoes.json";
+	public static void main(String[] args) throws IOException {
 
-		Menu menu = new Menu(caminhoArquivoJson);
-		menu.mostrarMenu();
-		
+		try {
+			String caminhoArquivoJson = "C:\\Users\\eliza\\eclipse-workspace\\anotacao\\anotacoes\\anotacoes.json";
+
+			Menu menu = new Menu(caminhoArquivoJson);
+			menu.mostrarMenu();
+
+		} catch (FileNotFoundException e) {
+			System.out.println("Arquivo inexistente");
+		} catch (IOException e) {
+			System.out.println("Problema no arquivo");
+		} catch (Exception e) {
+			System.out.println("Ocorreu erro");
+		}
+
 	}
 }
